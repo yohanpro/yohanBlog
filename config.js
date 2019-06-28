92a057818235375e2f6fcf9191794ec3bf21fc63
Yohan <yohan@yohanpro.com>
2019-06-28 16:02:50 +0900

    네트워크

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   copyright: "© All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: "UA-73379983-2",
+  googleAnalyticsId: "UA-140132541-1",
   menu: [
     {
       label: "Articles",
