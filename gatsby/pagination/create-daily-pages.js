aee7ae56950d0d54a682a1a01b113e25f0173d37
Yohan <yohan@yohanpro.com>
2020-10-05 14:00:04 +0900

    일상생활 및 취미 페이지에서 페이지네이션 오류 수정

--- gatsby/pagination/create-daily-pages.js
+++ gatsby/pagination/create-daily-pages.js
@@ -14,19 +14,19 @@ module.exports = async (graphql, actions) => {
     }
   `);
 
-  const { postsPerPage, dailyPostPerPage } = siteConfig;
+  const { dailyPostPerPage } = siteConfig;
   const numPages = Math.ceil(result.data.allMarkdownRemark.totalCount / dailyPostPerPage);
 
   for (let i = 0; i < numPages; i += 1) {
     createPage({
-      path: i === 0 ? '/normal' : `/normal/page/${i}`,
+      path: i === 0 ? '/normal' : `/normal/${i}`,
       component: path.resolve('./src/templates/daily-template.js'),
       context: {
         currentPage: i,
         postsLimit: dailyPostPerPage,
         postsOffset: i * dailyPostPerPage,
-        prevPagePath: i <= 1 ? '/' : `/page/${i - 1}`,
-        nextPagePath: `/page/${i + 1}`,
+        prevPagePath: i <= 1 ? '/' : `/normal/${i - 1}`,
+        nextPagePath: `/normal/${i + 1}`,
         hasPrevPage: i !== 0,
         hasNextPage: i !== numPages - 1
       }
